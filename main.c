c0fdf2f8334e80ce466c461e70bcb918a06b29a7
U-LAPTOP-8E2SPAP7\damad <ethanmcfadden101@gmail.com>
2026-03-01 15:23:37 -0800

    fixed a bunch of typos from copy/paste error. adding sound soon

--- main.c
+++ main.c
@@ -17,16 +17,16 @@
 
 //Function from tutorial that "maps values" in the function after this one?
 long Map(long value,long fromLow,long fromHigh,long toLow,long toHigh){
-    return (toHigh-toLow)*(value-fromLow) / (fromHigh-fromLow) + toLow;
+	return (toHigh-toLow)*(value-fromLow) / (fromHigh-fromLow) + toLow;
 }
 
 //Function from tutorial that can write to servo angle
 void setAngle(int pin, int angle){    //Create a funtion to control the angle of the servo.
-    if(angle < 0)
-        angle = 0;
-    if(angle > 180)
-        angle = 180;
-    softPwmWrite(pin,Map(angle, 0, 180, 5, 25));
+	if(angle < 0)
+		angle = 0;
+	if(angle > 180)
+		angle = 180;
+	softPwmWrite(pin,Map(angle, 0, 180, 5, 25));
 }
 
 
@@ -56,57 +56,54 @@ int main(void){
 
 	while(1){
 
-	//BUTTON
+		//BUTTON
 		// Indicate that button has pressed down
 		if(digitalRead(button) == 0){
 			// Led on
 			//digitalWrite(led1, LOW);
 			//Can add different functionality here
-			digitalWrite(laser_pin, HIGH);
+			digitalWrite(laserPin, HIGH);
 			//  printf("...LED on\n");
 		}
 		else{
 			// Led off
-		//	digitalWrite(led1, HIGH);
-			digitalWrite(laser_pin, HIGH);
+			//	digitalWrite(led1, HIGH);
+			digitalWrite(laserPin, LOW);
 			//Can add different functionality here
 
 			//  printf("LED off...\n");
 		}
 
-	//TILT SWITCH
-		
-		if(0 == digitalRead(TiltPin)){ //Read of Zero means tilted
-		    delay(10);
-            if(0 == digitalRead(TiltPin)){
-                printf("Tilt!\n");
+		//TILT SWITCH
+
+		if(0 == digitalRead(tilt)){ //Read of Zero means tilted
+			delay(10);
+			if(0 == digitalRead(tilt)){
+				printf("Tilt!\n");
 				//Add functionality with motor here
 			}
-        }
-        else if(1 == digitalRead(TiltPin)){ //Read of One means not tilted
-            delay(10);
-            if(1 == digitalRead(TiltPin)){
-                printf("Not Tilting!\n");
+		}
+		else if(1 == digitalRead(tilt)){ //Read of One means not tilted
+			delay(10);
+			if(1 == digitalRead(tilt)){
+				printf("Not Tilting!\n");
 				//Add functionality with motor here
-            
+
 			}
-        }
+		}
 
-	//SERVO 
-		for(i=0;i<181;i++){     // Let servo rotate from 0 to 180.                  
+		//SERVO 
+		for(int i=0;i<181;i++){     // Let servo rotate from 0 to 180.                  
 			setAngle(ServoPin,i); //change specifics for our needs
-            delay(2);
-        }
-        delay(1000);
-        for(i=181;i>-1;i--){        // Let servo rotate from 180 to 0.              
+			delay(2);
+		}
+		delay(1000);
+		for(int i=181;i>-1;i--){        // Let servo rotate from 180 to 0.              
 			setAngle(ServoPin,i); //change specifics for our needs
-            delay(2);
-        }
-        delay(1000);
-    }
-
+			delay(2);
+		}
+		delay(1000);
 	}
 
-}
 return 0;
 }
